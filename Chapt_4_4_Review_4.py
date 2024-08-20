4b3c10a63481aa74f538a14a21262fd18c611738
jamesdenney73 <jamesdenney73@gmail.com>
2024-08-19 20:03:50 -0500

    startwidth

---
+++ Chapt_4_4_Review_4.py
@@ -0,0 +1,12 @@
+string1 = "Becomes"
+string2 = "becomes"
+string3 = "BEAR"
+string4 = "     Beautiful"
+print ("Does " + string1 + " start with be?") 
+print (string1.startswith("be"))
+print ("Does " + string2 + " start with be?") 
+print (string2.startswith("be"))
+print ("Does " + string3 + " start with be?") 
+print (string3.startswith("be"))
+print ("Does " + string4 + " start with be?") 
+print (string4.startswith("be"))
