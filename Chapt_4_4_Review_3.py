864f4e592b956161a897db4e0ffd8ef8a098b216
jamesdenney73 <jamesdenney73@gmail.com>
2024-08-22 18:12:15 -0500

    next

---
+++ Chapt_4_4_Review_3.py
@@ -0,0 +1,5 @@
+#write a program that accepts input and returns the nubmer of charaters entered
+text = "type a string"
+user_input = input("Prompt: ")
+number_characters = len(user_input)
+print (number_characters)
