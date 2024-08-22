63cb2f39757a9bd79f8999fa44681d6a6dfc5335
jamesdenney73 <jamesdenney73@gmail.com>
2024-08-22 18:08:04 -0500

    next

---
+++ Chapt_4_4_Review_2.py
@@ -0,0 +1,5 @@
+# write a program that accepts input from a user and write as lower case
+question = "What would you like whispered? "
+user_input = input ("Yell what you want whispered:")
+whisper = user_input.lower()
+print (question + whisper)
