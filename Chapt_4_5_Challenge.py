3146bf6297778d72342e5cd5864ee5fc289a0d27
jamesdenney73 <jamesdenney73@gmail.com>
2024-08-22 19:16:05 -0500

    done

--- Chapt_4_5_Challenge.py
+++ Chapt_4_5_Challenge.py
@@ -1,3 +1,7 @@
-#Writ a program named first_letter.py that prompt the user for imput with the string
+# Writ a program named first_letter.py that prompt the user for imput with the string
 # "Tell me your password". The program should then determine the first letter of the
-# users imput, convert that letter to upper case and display it back 
+# users imput, convert that letter to upper case and display it back.
+user_input = input("Pleaes enter your password Makesure its fictitious: ")
+first_letter = user_input[0]
+first_letter = first_letter.upper()
+print (first_letter)
