3ebf1c281788433abc77f5f6132bed7ad143eeb1
jamesdenney73 <jamesdenney73@gmail.com>
2024-08-22 19:41:49 -0500

    print calculations and text

---
+++ Chapt_4_6_Review_4.py
@@ -0,0 +1,5 @@
+number1 = input("Please enter a number between 1 and 10: ")
+number2 = input("Please enter a second number between 1 and 10: ")
+number3 = int(number1)
+number4 = int(number2)
+print("The product of " + number1 + " and " + number2 + " is " + str(number3 * number4))
