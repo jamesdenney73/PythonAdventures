1afb70c4966e0e63a8d653c296a5f259bed927aa
jamesdenney73 <jamesdenney73@gmail.com>
2024-08-28 18:49:51 -0500

    find a

---
+++ Chapt 4-8a Review 1.py
@@ -0,0 +1,7 @@
+#The following 2 lines work but is not what is asked for. 
+#The task is to do it on a single line
+#text = "AAA" 
+#print (text.find("a"))
+# I have to concat AAA into a string object by concat it with ""
+print("AAA".find('a'))
+
