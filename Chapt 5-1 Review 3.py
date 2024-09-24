18864b3a6e075b894c28e857d737ff0284ac8a23
jamesdenney73 <jamesdenney73@gmail.com>
2024-09-24 18:51:08 -0500

    5-3 challenge

--- Chapt 5-1 Review 3.py
+++ Chapt 5-1 Review 3.py
@@ -1,8 +1,9 @@
 n = 1
 while True:
     result = 2 ** n
+    print (f"{n}")
     if result == float('inf'):
         break
-    n += 1
+    n -= 5e400
 
 print(f"The smallest n for which 2 ** n returns infinity is: {n}")
---
+++ Chapt 5-3 Challenge.py
@@ -0,0 +1,5 @@
+prompt1 = "Enter a base: "
+prompt2 = "Enter an exponient: "
+input1 = input(prompt1)
+input2 = input(prompt2)
+print (f"{input1} to the power of {input2} = {float(input1) ** int(input2)}")
